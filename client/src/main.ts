29c710e62ecdc3fea845f304b56edbdd70b2b167
GenceErdagi <70435995+GenceErdagi@users.noreply.github.com>
2024-05-19 19:32:17 +0300

    Client change to vanilla-ts Vite

---
+++ client/src/main.ts
@@ -0,0 +1,13 @@
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import { AppRouter } from '../../server/routers/index';
+
+const client = createTRPCProxyClient<typeof AppRouter>({
+	links: [httpBatchLink({ url: 'http://localhost:8000/trpc' })]
+});
+
+async function main() {
+	const result = await client.users.adminOnlyProcedure.query();
+	console.log(result);
+}
+
+main();
